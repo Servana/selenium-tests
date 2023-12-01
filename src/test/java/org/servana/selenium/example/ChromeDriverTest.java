7a5e1a34b5c36adc022bee883c580ba1ead17ba3
DhruvJ225 <dhruv@servanamanaged.com>
2023-12-01 11:43:44 +0530

    Add test script for selenium ui tests

---
+++ src/test/java/org/servana/selenium/example/ChromeDriverTest.java
@@ -0,0 +1,51 @@
+package org.servana.selenium.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ChromeDriverTest {
+
+    private String testUrl;
+    private WebDriver driver;
+
+    @Before
+    public void prepare() {
+        //setup chromedriver
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "/usr/bin/google-chrome");
+        System.setProperty("DISPLAY", ":99");
+        environment.put("DISPLAY", ":99");
+        testUrl = "https://google.com";
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+
+        driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+
+        driver.get(testUrl);
+
+    }
+
+    @Test
+    public void testTitle() throws IOException {
+
+        List<WebElement> elements = driver
+                .findElements(By.cssSelector("[lang=\"READ_MORE_BTN\"]"));
+
+    }
+
+    @After
+    public void teardown() throws IOException {
+        driver.quit();
+    }
+
+}
